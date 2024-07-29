82bc0307081904ace67ef05d3f5faea56100afb4
adithyan10210704 <162334111+adithyan10210704@users.noreply.github.com>
2024-07-29 14:30:45 +0530

    Add files via upload

---
+++ No_of_occurences.py
@@ -0,0 +1,8 @@
+def count_no_of_times(line,word):
+    words=line.split()
+    count=words.count(word)
+    return count
+line="This is a test A sample test for python coding test"
+word="test"
+no_of_times=count_no_of_times(line,word)
+print(f"The word {word} occurs {no_of_times} times in the line.")
