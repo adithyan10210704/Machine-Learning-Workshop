86776da46dfca06a4ddb37c2cf4af1f38dea27fd
adithyan10210704 <162334111+adithyan10210704@users.noreply.github.com>
2024-07-29 13:24:25 +0530

    Add files via upload

---
+++ Student details - dictionary.py
@@ -0,0 +1,8 @@
+student ={"name":"John","age":25,"courses":["Math","Science"]}
+print(student["name"])
+student["age"]=26
+student["grade"]="A"
+del student["age"]
+print(student["grade"])
+student["age"]=20
+print(student["age"])
