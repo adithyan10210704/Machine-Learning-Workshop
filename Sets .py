2b74ae863263f741accb48d0dc9ec64e2d932ff3
adithyan10210704 <162334111+adithyan10210704@users.noreply.github.com>
2024-07-29 13:39:50 +0530

    Add files via upload

---
+++ Sets .py
@@ -0,0 +1,10 @@
+numbers={1,2,3,4,5}
+numbers.add(6)
+numbers.remove(3)
+evens={2,4,6,8}
+union=numbers | evens
+intersection = numbers & evens
+difference= numbers-evens
+print(intersection)
+print(difference)
+print(union)
